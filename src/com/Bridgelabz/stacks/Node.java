78d44e0de007860b5c8f8d73bec2a52e0b7cf271
pratikraut25 <pratikraut2512@gmail.com>
2022-11-12 14:15:02 +0530

    createdlist

---
+++ src/com/Bridgelabz/stacks/Node.java
@@ -0,0 +1,27 @@
+package com.Bridgelabz.stacks;
+
+public class Node <T> {
+	private T key;
+    private Node<T> next;
+
+    public Node(T key) {
+        this.key = key;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+}
